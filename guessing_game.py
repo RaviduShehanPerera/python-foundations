563753d81a44738651ee60a9b16931c3789ceb72
Ravidu Perera <shehanperera1999@gmail.com>
2026-06-20 18:43:04 -0400

    Week 2 Day 4: number guessing game

---
+++ guessing_game.py
@@ -0,0 +1,32 @@
+import random 
+
+print ("===Number Guessing Game===")
+print ("I am thinking of a number between 1 and 100")
+
+number = random.randint(1,100)
+attempts = 0
+
+while True:
+    guess = input("what is your guess? ")
+    guess = int(guess)
+    attempts += 1
+
+    if guess == number:
+       break 
+    elif guess > number:
+
+        print (f"Guess ={guess}")
+        print ("Too high!")
+
+    elif guess < number:
+        print (f"Guess : {guess}")
+        print ("Too low!")    
+
+print(f"correct!, you got it in {attempts} attempts! ")    
+
+if attempts <= 5:
+    print ("Amazing!")
+elif attempts  <=10:
+    print("Good job!")
+elif attempts > 11:
+    print("You got there eventually!")    
